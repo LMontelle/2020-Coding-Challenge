75d9a3ace167d219ed3278750099119e687e4f62
LMontelle <158831443+LMontelle@users.noreply.github.com>
2025-04-04 20:29:05 -0400

    Editing Permissions

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 #to ensure the correct comparisons are being made to scoreboard
 from copy import deepcopy
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/static/*": {"origins": "*"}})
 
 scoreboard = [
     {
