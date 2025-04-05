3b0b9bec48b03b449462e8ef5ac80fe79c1b4f89
LMontelle <158831443+LMontelle@users.noreply.github.com>
2025-04-04 20:12:40 -0400

    Print Statements for Debugging

--- server.py
+++ server.py
@@ -67,6 +67,8 @@ def increase_score():
 
     json_data = request.get_json()   
     team_id = json_data["id"]  
+
+    print(f"Received request to increase score for team ID: {team_id}")
     
     for team in scoreboard:
         if team["id"] == team_id:
